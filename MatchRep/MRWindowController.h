50864cffdee8115be940363de85ede4af566b684
tahoupt <tom@houptlab.org>
2022-10-21 17:03:27 -0400

    parse PMID and add button to rename pdf file

--- MatchRep/MRWindowController.h
+++ MatchRep/MRWindowController.h
@@ -11,12 +11,16 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface MRWindowController : NSWindowController
+@interface MRWindowController : NSWindowController <WKNavigationDelegate>
 
 @property NSURL *pdfURL;
 @property PDFDocument *pdf;
 @property IBOutlet PDFView *pdfView;
 @property IBOutlet WKWebView *webView;
+@property IBOutlet NSTextField *textField;
+@property IBOutlet NSTextField *pmidField;
+@property IBOutlet NSButton *renameButton;
+
 
 -(void)displayPDF;
 
--- MatchRep/MRWindowController.m
+++ MatchRep/MRWindowController.m
@@ -35,4 +35,64 @@
     
 }
 
+-(void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation; {
+
+    [_textField setStringValue:[[webView URL] absoluteString]];
+    
+   NSString *pmidStr = [[_webView URL] lastPathComponent];
+   
+   NSError *error = NULL;
+NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"\\b[0-9]+\\b"
+                                                                       options:NSRegularExpressionCaseInsensitive
+                                                                         error:&error];
+
+NSUInteger numberOfMatches = [regex numberOfMatchesInString:pmidStr
+                                                    options:0
+                                                      range:NSMakeRange(0, [pmidStr length])];
+
+
+if ([pmidStr length] >= 7 && numberOfMatches == 1) {
+    // this matches the criteria
+    
+    [_pmidField setStringValue:pmidStr];
+    [_renameButton setEnabled:YES];
+    
+    
+} else {
+
+    [_renameButton setEnabled:NO];
+}
+
+}
+
+-(IBAction)renamePDF:(id)sender; {
+
+// get pmid from last element of [_textField setStringValue]
+// replace last element of _pdfURL with lastElement of webView URL]
+NSString *pmid =  [_pmidField stringValue];
+
+
+NSString *pmidpdf = [pmid stringByAppendingString:@".pdf"];
+
+NSURL *newURL = [[_pdfURL URLByDeletingLastPathComponent] URLByAppendingPathComponent: pmidpdf];
+
+
+NSLog(@"New URL: %@",[newURL path]);
+
+NSError *error = nil;
+// source and destination are URLs
+//if ([_pdfURL startAccessingSecurityScopedResource]) {
+        [[NSFileManager defaultManager] moveItemAtURL:_pdfURL toURL:newURL error:&error];
+       // copyItemAtURL or moveItemAtUrl throws a 513 error!
+       // do turned off sandbocing in MatchRep.entitlements
+       
+       //try  https://tw.coderbridge.com/questions/cb9c272df6424692b3a25bbe7c152d81
+       // [_pdfURL setResourceValue:pmidpdf forKey:NSURLNameKey error:&error];
+        
+//}
+[_pdfURL stopAccessingSecurityScopedResource];
+
+
+}
+
 @end
